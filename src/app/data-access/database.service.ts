bbb251744f44ec76c19679b56777f294ab8329b2
Marc Lammers <lammers.marc@gmail.com>
2019-10-05 20:25:44 +0200

    Create database.service.ts

---
+++ src/app/data-access/database.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {Connection, ConnectionOptions, createConnection} from 'typeorm';
+
+import { Settings } from './databaseConfig';
+
+@Injectable()
+export class DatabaseService {
+    public connection: Promise<Connection>;
+    private readonly options: ConnectionOptions;
+
+    constructor() {
+        Settings.initialize();
+        this.options = {
+            type: 'sqlite',
+            database: Settings.dbPath,
+            entities: [],
+            migrationsRun: true,
+            migrations: ['./migrations'],
+            synchronize: true,
+            logging: 'all',
+        };
+        this.connection = createConnection(this.options);
+    }
+}
