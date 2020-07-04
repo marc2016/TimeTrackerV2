46f3fa2bc7036e2b3185e4433f4dd2e4cd13edf2
Marc Lammers <marc@lammers.dev>
2020-07-04 23:21:15 +0200

    Project refresh and first steps.

--- src/app/views/timerlist/timerlist.component.ts
+++ src/app/views/timerlist/timerlist.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  templateUrl: 'timerlist.component.html'
+  templateUrl: "timerlist.component.html",
+  styleUrls: ["./timerlist.component.scss"],
 })
 export class TimerlistComponent implements OnInit {
-
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 }
--- src/app/views/timerlist/timerlist.module.ts
+++ src/app/views/timerlist/timerlist.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { TimerlistComponent } from './timerlist.component'
 import { TimerlistRoutingModule } from './timerlist-routing.module';
+import { TimeEntryComponent } from './time-entry/time-entry.component';
 
 @NgModule({
   imports: [
       TimerlistRoutingModule
   ],
-  declarations: [ TimerlistComponent ]
+  declarations: [ TimerlistComponent, TimeEntryComponent ]
 })
 export class TimerlistModule { }
