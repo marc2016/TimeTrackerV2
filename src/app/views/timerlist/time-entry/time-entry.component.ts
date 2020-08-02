30f2508a2c03ddb8adeadb5012be05eb29557523
Marc Lammers <marc@lammers.dev>
2020-08-02 08:09:55 +0200

    First design of todolist item added.

--- src/app/views/timerlist/time-entry/time-entry.component.ts
+++ src/app/views/timerlist/time-entry/time-entry.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-time-entry',
-  templateUrl: './time-entry.component.html',
-  styleUrls: ['./time-entry.component.scss']
+  selector: "app-time-entry",
+  templateUrl: "./time-entry.component.html",
+  styleUrls: ["./time-entry.component.scss", "./checkbox.css"],
 })
 export class TimeEntryComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  clickCheckbox() {
+    console.log("TEST");
   }
-
 }
